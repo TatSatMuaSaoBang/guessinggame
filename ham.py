8a997015fb137dc331a64f98ea6f4e09b3ef62df
TatSatMuaSaoBang <chickenniggen568@gmail.com>
2023-10-27 16:25:25 +0700

    you mom fat

--- ham.py
+++ ham.py
@@ -1,22 +1,23 @@
 # Finding number require
 def finding_number(Element_to_find, random_number):
     return random_number.index(Element_to_find)
-random_number=[2,3,4,5,6]
-answear=""
-random=[]
-for i in range(0,len(random_number)):
-    random.append("#")
-    answear = answear + str(random_number[i])
+def finding_thatonenumber(list, dauvao):
+    numberlist=[]
+    for i in range(0,len(list)):
+        if dauvao == list[i]:
+            numberlist.append(i)
+    return numberlist
 
 def finding_numberhidden(random_number, dauvao):
-    random=[]
-    for i in range(0,len(random_number)):
-        random.append("#")
-    for i in range(0,len(dauvao)):
+    random = ["#"] * len(random_number)  # Sử dụng list comprehension để tạo danh sách "#" ban đầu
+    for i in range(len(dauvao)):
         if dauvao[i] in random_number:
-            idk=finding_number(dauvao[i], random_number)
+            idk = finding_number(dauvao[i], random_number)
             if dauvao[i] in random:
-                random
+                a = finding_thatonenumber(random, dauvao[i])
+                if a:  # Kiểm tra nếu danh sách chỉ số không rỗng
+                    b = a[0] + 1  # Lấy chỉ số tiếp theo
+                    random[b] = dauvao[i]
             else:
                 random[idk] = dauvao[i]
     return random
